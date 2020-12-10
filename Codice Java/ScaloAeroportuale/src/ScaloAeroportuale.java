fb8df99e8ed7ac03f633a88479ebe48ca30ac2e8
_Mathaus <64974057+ColdMathaus@users.noreply.github.com>
2020-12-10 18:52:46 +0100

    Update
    Co-Authored-By: MariaelenaCiccarelli <75440767+MariaelenaCiccarelli@users.noreply.github.com>

--- Codice Java/ScaloAeroportuale/src/ScaloAeroportuale.java
+++ Codice Java/ScaloAeroportuale/src/ScaloAeroportuale.java
@@ -1,6 +1,17 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.AbstractListModel;
+import javax.swing.JScrollBar;
+import javax.swing.JSpinner;
+import javax.swing.JTree;
+import javax.swing.JSlider;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class ScaloAeroportuale {
 
@@ -37,6 +48,15 @@ public class ScaloAeroportuale {
 		frmScaloAeroportuale.setTitle("Scalo Aeroportuale");
 		frmScaloAeroportuale.setBounds(100, 100, 721, 502);
 		frmScaloAeroportuale.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmScaloAeroportuale.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Scegliere Aeroporto in cui lavorare");
+		lblNewLabel.setBounds(84, 220, 181, 31);
+		frmScaloAeroportuale.getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setSelectedIndex(2);
+		comboBox.setBounds(349, 196, 122, 22);
+		frmScaloAeroportuale.getContentPane().add(comboBox);
 	}
-
 }
